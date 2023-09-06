de5b9fd3abe8b0c08135fb7ae4ef9c18c3db9adb
RobertZakoyan <114802387+RobertZakoyan@users.noreply.github.com>
2023-09-06 15:52:37 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+import sys
+import geopandas as gpd
+import fiona
+import os
+import pandas as pd
+
+fiona.supported_drivers['KML'] = 'rw'
+my_db_file = r"c:\Users\Robert\Desktop\small.sqlite"
+
+def main():
+    gdf = gpd.read_file(sys.argv[1])
+    file_name = os.path.basename(sys.argv[1]).split('.')[0]
+    print(file_name)
+    if sys.argv[3] == "csv":
+        gdf.to_csv(f"c:\\Users\\Robert\\Desktop\\Geo_data\\{file_name}.csv")
+    elif sys.argv[3] == 'kml':
+        gdf.to_file(f"c:\\Users\\Robert\\Desktop\\Geo_data\\{file_name}.kml", driver = 'KML', CRS = 'EPSG:4326')
+    elif sys.argv[3] == 'xlsx':
+        df = pd.DataFrame(gdf)
+        df.to_excel(f"c:\\Users\\Robert\\Desktop\\Geo_data\\{file_name}.xlsx")
+if __name__ == '__main__':
+    main()
+
