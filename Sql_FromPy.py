03680bf9c8c42f54d339fa0cc561ec7f07521ba9
RobertZakoyan <mafrobo@gmail.com>
2023-09-08 21:51:15 +0400

    Some changes

--- Sql_FromPy.py
+++ Sql_FromPy.py
@@ -9,22 +9,22 @@ conn = None
 cur = None  
 table_read = "SELECT * FROM road_data"
 table_create = "CREATE TABLE locations_table(id INT PRIMARY KEY, x_coordinate DOUBLE PRECISION, y_coordinate DOUBLE PRECISION, geom GEOMETRY)"
-try:
-    conn = psycopg2.connect(database=con_database, user="postgres", password="masis22016", host="localhost", port="5432")
-
+def main():
+    try:
+        cur.execute(table_create)
+        conn.commit()
+        
+    except:
+        raise Exception("Sorry, you have problem in your code")
+    finally:
+        cur.close()
+        conn.close()
+    print("hello1")
+if __name__ == "__main__":
+    conn = psycopg2.connect(database="postgres", user="postgres", password="masis22016", host="localhost", port="5432")
     cur = conn.cursor()
 
-    cur.execute(table_create)
-    conn.commit()
-    
-except:
-    raise Exception("Sorry, you have problem in your code")
-finally:
-    cur.close()
-    conn.close()
-print("hello1")
-# if __name__ == "__main__":
-#     table_create = "CREATE TABLE public.locations_table(id INT PRIMARY KEY, x_coordinate DOUBLE PRECISION, y_coordinate DOUBLE PRECISION, geom GEOMERTY)"
-    
-    
-#     main()
+    table_create = "CREATE TABLE locations_table(id INT PRIMARY KEY, x_coordinate DOUBLE PRECISION, y_coordinate DOUBLE PRECISION, geom geometry)"
+    main()
+print("helloas")
+print("helloas")
